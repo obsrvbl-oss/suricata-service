e67c23d4607313886a53bd569e304095888e5674
Stephen Zimmerman <szimmer@obsrvbl.com>
2015-03-11 12:04:24 -0500

    Now functional.

---
+++ packaging/scripts/postinst.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+useradd --create-home --home-dir /opt/suricata suricata
+mkdir -p /opt/suricata/logs
+cp /etc/nsm/templates/suricata/* /opt/suricata
+chown -R suricata /opt/suricata
+
+SURICATA=/usr/bin/suricata
+chown suricata $SURICATA
+chmod 750 $SURICATA
+setcap cap_net_raw,cap_net_admin=eip $SURICATA
