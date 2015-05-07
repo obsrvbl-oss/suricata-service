65aa845f3b6d43cc72054f0fea99a232237da272
Bo Bayles <bbayles@gmail.com>
2015-05-07 14:51:50 -0500

    Use the main directory for rules

--- packaging/scripts/postinst.sh
+++ packaging/scripts/postinst.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
-mkdir -p /opt/suricata
+BINARY_PATH=/usr/bin/suricata
+SURICATA_DIR=/opt/suricata
+
+# Create a suricata system user and group with the given home directory
+mkdir -p $SURICATA_DIR
+
 adduser \
 --quiet \
 --system \
 --no-create-home \
 --group \
 --disabled-password \
---home /opt/suricata \
+--home $SURICATA_DIR \
 suricata
 
-mkdir -p /opt/suricata/logs
-cp /etc/nsm/templates/suricata/* /opt/suricata
-chown -R suricata /opt/suricata
+# Create locations for rules, logs, and templates
+mkdir -p $SURICATA_DIR/rules
+mkdir -p $SURICATA_DIR/logs
+cp /etc/nsm/templates/suricata/*.config $SURICATA_DIR
 
-SURICATA=/usr/bin/suricata
-chown suricata $SURICATA
-chmod 750 $SURICATA
-setcap cap_net_raw,cap_net_admin=eip $SURICATA
+# Set permissions
+chown -R suricata:suricata $SURICATA_DIR
+chown suricata:suricata $BINARY_PATH
+chmod 750 $BINARY_PATH
+setcap cap_net_raw,cap_net_admin=eip $BINARY_PATH
