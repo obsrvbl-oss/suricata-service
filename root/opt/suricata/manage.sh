3938d3b3ada47803305dddb9fb401002a8cb9f24
Stephen Zimmerman <szimmer@obsrvbl.com>
2015-06-26 11:59:53 -0500

    Add a command to reload the config.  Extends the "logcycle" script to something a little smarter,  so I renamed it.

---
+++ root/opt/suricata/manage.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+LOG_FILE=/opt/suricata/logs/eve.json
+DATE=$(date +%s)
+PID=$(pidof "/usr/bin/suricata")
+
+action=$1
+case $action in
+	cycle-logs)
+		# Exit early if the log file doesn't exist
+		if [ ! -e $LOG_FILE ]
+		then
+		    echo "$LOG_FILE does not exist"
+		    exit 0
+		fi
+
+		# Also exit early if the log file is 0 bytes
+		if [ ! -s $LOG_FILE ]
+		then
+		    echo "$LOG_FILE is 0 bytes"
+		    exit 0
+		fi
+
+
+		# Rename the current log file and then send suricata a
+		# HUP signal to tell it to open a new log file.
+		mv $LOG_FILE $LOG_FILE.$DATE.archived
+		/bin/kill -HUP $PID
+		;;
+
+	reload-config)
+		# SIGUSR2 == suricata reloads its configuration
+		/bin/kill -USR2 $PID
+		;;
+
+	*)
+		echo "bad action '$action'. Try: cycle-logs, reload-config"
+		echo "   ex: $0 reload-config"
+		exit 2
+esac
+
