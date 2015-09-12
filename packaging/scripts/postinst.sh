be6c2f4f40d2ae8affd0807900b863bbb4b745a9
Bo Bayles <bbayles@gmail.com>
2015-09-11 21:35:22 -0500

    Remove Debianisms

--- packaging/scripts/postinst.sh
+++ packaging/scripts/postinst.sh
@@ -7,29 +7,31 @@ SURICATA_DIR=/opt/suricata
 # Create a suricata system user and group with the given home directory
 mkdir -p $SURICATA_DIR
 
-adduser \
-    --quiet \
+useradd \
     --system \
+    --user-group \
     --no-create-home \
-    --group \
-    --disabled-password \
-    --home $SURICATA_DIR \
+    --home-dir $SURICATA_DIR \
+    --shell /bin/false \
     suricata
-
-# If the ona-service package (recommended) is installed, add the obsrvbl_ona
-# user to the suricata group so it can read alert logs.
-addgroup obsrvbl_ona suricata
 
 # Create locations for rules, logs, and templates
 mkdir -p $SURICATA_DIR/rules
 mkdir -p $SURICATA_DIR/logs
 cp /etc/suricata/*.config $SURICATA_DIR
 
+# If the ona-service package (recommended) is installed, add the obsrvbl_ona
+# user to the suricata group so it can read alert logs. Also allow it to
+# update rules automatically.
+if getent passwd | grep -q "^obsrvbl_ona:"; then
+    usermod -a -G suricata obsrvbl_ona
+    chown -R obsrvbl_ona $SURICATA_DIR/rules
+fi
+
 # Set permissions
 chown -R suricata:suricata $SURICATA_DIR
-chown -R obsrvbl_ona $SURICATA_DIR/rules
 chown suricata:suricata $BINARY_PATH
 chmod 0750 $BINARY_PATH
 chmod 0754 $SURICATA_DIR/manage.sh
 chmod g+w $SURICATA_DIR/logs
-setcap cap_net_raw,cap_net_admin=eip $BINARY_PATH
+setcap cap_net_raw,cap_net_admin=eip $BINARY_PATH=
